7c808713f88f1713df0bdf55f6555763b5bd4630
GrzegorzZuk <grzuk7@gmail.com>
2020-06-02 15:49:23 +0200

    v1.1

--- weiCalc.js
+++ weiCalc.js
@@ -3,10 +3,23 @@ function calcWeights() {
     document.getElementById("demo").innerHTML = String(tWei);
     var table = document.getElementById('weights');
     var row = table.insertRow(table.rows.length);
+    var idx = table.rows.length;
+    var delBtn = document.createElement("button");
+    delBtn.appendChild(document.createTextNode("delete entry"));
+    row.id = "row" + String(idx);
+    delBtn.setAttribute('index', String(idx));
+    delBtn.addEventListener('click', deleteRow);
+    // delBtn.addEventListener("click", deleteRow);
+    var del = row.insertCell(0);
+    del.appendChild(delBtn);
     row.insertCell(0).innerHTML = String((tWei * 0.8).toFixed(1));
     row.insertCell(0).innerHTML = String((tWei * 0.6).toFixed(1));
     row.insertCell(0).innerHTML = String((tWei * 0.4).toFixed(1));
     row.insertCell(0).innerHTML = String(tWei);
+}
+function deleteRow(btn) {
+    var row = document.getElementById("row" + btn.target.getAttribute('index'));
+    row.parentNode.removeChild(row);
 }
 document.getElementById('sBtn').addEventListener("click", function () {
     calcWeights();
--- weiCalc.ts
+++ weiCalc.ts
@@ -4,11 +4,27 @@ function calcWeights() {
 
     let table = document.getElementById('weights') as HTMLTableElement;
     let row = table.insertRow(table.rows.length);
+    let idx: number = table.rows.length;
     
+    let delBtn = document.createElement("button");
+    delBtn.appendChild(document.createTextNode("delete entry"));
+    row.id = "row"+String(idx);
+    delBtn.setAttribute('index', String(idx));
+    delBtn.addEventListener('click', deleteRow);
+    // delBtn.addEventListener("click", deleteRow);
+
+    let del = row.insertCell(0);
+    del.appendChild(delBtn);
     row.insertCell(0).innerHTML = String((tWei * 0.8).toFixed(1));
     row.insertCell(0).innerHTML = String((tWei * 0.6).toFixed(1));
     row.insertCell(0).innerHTML = String((tWei * 0.4).toFixed(1));
     row.insertCell(0).innerHTML = String(tWei);
+}
+
+function deleteRow(btn) {
+    let row = document.getElementById("row"+btn.target.getAttribute('index'))
+    row.parentNode.removeChild(row);
+    
 }
 
 document.getElementById('sBtn').addEventListener("click", () => {
